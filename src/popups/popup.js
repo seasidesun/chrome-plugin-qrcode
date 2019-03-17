1c0ced7119432d237ca2c73a0313c1aa860b7ac4
haibinsun <seasidetank@gmail.com>
2019-03-17 12:19:22 +0800

    feat: modify the qrcode generate

--- src/popups/popup.js
+++ src/popups/popup.js
@@ -1,15 +1,49 @@
 window.onload = function () {
-    //https://github.com/davidshimjs/qrcodejs
+    // https://github.com/davidshimjs/qrcodejs
     var defaultConfig = {
-        text: 'https://github.com/davidshimjs/qrcodejs',
+        text: '',
         width: 230,
         height: 230,
-        colorDark : '#000000',
-        colorLight : '#ffffff'
+        colorDark: '#000000',
+        colorLight: '#ffffff',
+        correctLevel: QRCode.CorrectLevel.H
     }
-    new QRCode(document.getElementById('qrcode'), defaultConfig);
-    chrome.tabs && chrome.tabs.getSelected && chrome.tabs.getSelected(null, function (tab) { 
-        defaultConfig.text = tab.url;
-        new QRCode(document.getElementById('qrcode'), defaultConfig);
+    var getCurrent = chrome.tabs && chrome.tabs.getSelected && chrome.tabs.getSelected || function () {}
+
+    genQrcode()
+
+    var urlBox = document.getElementById('text-url');
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode != 13) return;
+        e.preventDefault();
+
+        genQrcode(urlBox.value)
+        autoSetBoxHeight(urlBox)
     });
+
+    function genQrcodeOfCur () {
+        getCurrent(function (tab) {
+            if (!tab || !tab.url) return
+            genQrcode(tab.url)
+        });
+    }
+
+    var qrcode
+    function genQrcode (url) {
+        if (!url) return genQrcodeOfCur()
+        else {
+            if (qrcode) {
+                qrcode.clear();
+                qrcode.makeCode(url);
+            } else {
+                defaultConfig.text = url;
+                qrcode = new QRCode(document.getElementById('qrcode'), defaultConfig);
+            }
+        }
+    }
+
+    function autoSetBoxHeight (box) {
+        box.style.height = 'auto'
+        box.style.height = box.scrollHeight + 'px';
+    }
 }
